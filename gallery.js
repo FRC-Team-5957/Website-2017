8b65423af35a8b2cb49f4955264822cd0b006754
Aaron Greco <mbqwertyaaa@gmail.com>
2018-02-21 11:52:23 -0500

    Updated Gallery and Gallery Content
    Formatted pictures and added captions.
    Removed redundant part of gallery call function, this part could be done inside the function. Also added more comments on the functions in gallery.js #16
    Barely started work on moving captions. May have to change systems to do what is needed for caption changing.

--- gallery.js
+++ gallery.js
@@ -1,6 +1,7 @@
 "use strict";
-//gallery(Directory, width of page, total number of pictures in directory index from 0, extension as .jpg or .png)
+//gallery(directory, width of page, total of files in directory retireved automatically, extension as .jpg or .png)
 function modal() {
+  //writes in the modal section.
     document.write('<section id="modal" >');
     document.write('<section id="modal-container">');
     document.write('<img src=""alt="" id="image">');
@@ -12,25 +13,29 @@ function modal() {
     document.write('</section>');
 }
 
-function gallery(dir, imax, ext, cap) {
+function gallery(dir, ext, cap) {
 //imax - Internal Maximum
     document.write('<gallery>');
     //Declare path and file variables. Path stores the file path, and File stores the path and the file name and extension.
+    var imax = cap.length;
     var path;
     var file;
     var capt;
-    var state = 0;
-    path = "gallery/" + dir + "/min/";
+    var path = "gallery/" + dir + "/min/";
     //Declare and set index to 0.
     var i = 0;
     //for loop from 0 to total number of files in the folder, adding 1 to index each time it's run.
     for (i = 0; i <= imax; i++) {
         //Combine path info into file variable.
         var file = (path + i + ext);
+        //Grab caption for this picture
         var capt = cap[i];
+        //adds parts of the onclick method together.
         var start = ('openModal(\"' + file + '\",\"' + capt + '\")');
+        //makes image and caption sections
         var imgplace = ('<img src="' + file + '" alt="' + capt + '">');
         var capplace = ('<p class="cap">' + capt + '</p>');
+        //writes different parts to file in place
         document.write('<section class=\"sect\" onclick=\"openModal(\'' + file + '\',\'' + capt.replace("'", "\\&apos;") + '\',\'' + imax +'\')\">');
         document.write(imgplace);
         document.write(capplace);
@@ -41,24 +46,27 @@ function gallery(dir, imax, ext, cap) {
 }
 
 function openModal(path, caption, imax) {
+  //change path from thumbnail to full size
     var fpath = path.replace("/min","");
     var elem = document.getElementById("modal");
     elem.style.display = "block";
     document.getElementById("image").src = fpath;
     document.getElementById("image").alt = caption;
     document.getElementById("modal-caption").innerHTML = caption;
+    //sets increase button function's maximum
     document.getElementById("modal-inc").setAttribute( "onClick", "javascript: incModal("+imax+");" );
     var body = document.getElementsByClassName("team");
     body.style[1] = "height:100%; overflow:auto; scroll:no;"
 }
 function incModal(imax) {
     var path = document.getElementById("image").src;
+    
     console.log(imax+' imax.');
     console.log(path+' Image Path');
-    
-    
+    /* //Starting cation code
+    var captionorigin = document.get
+    */
     var newpath = path.split("/");
-    
     var length = newpath.length-1;
     
     //image index string
@@ -66,7 +74,7 @@ function incModal(imax) {
     
     console.log(newpath[length]+' Last element of newpath array.');
     console.log(imIS[0]+' Image Index before increment');
-
+    //if current index is max, do nothing, else increase by one 
     if(imIS[0]==(imax-1)){
       var match = imIS[0]
       }
@@ -74,31 +82,26 @@ function incModal(imax) {
     var match = Number(imIS[0])+1;
     }
     
-    
     console.log(match+' match');
-    // "/.$/" Replaces last character of string with specified string.
-    //var gpath = epath.replace(/.$/,String(match))+".jpg";
+    //join path to put into file
     imIS[0] = match;
     newpath[length]=imIS.join(".")
     var gpath = newpath.join("/")
-    console.log(gpath+' Final Path');
     
+    console.log(gpath+' Final Path');
+    //write path
     var caption = document.getElementById("image").alt;
     console.log(caption);
-    var doc = document.getElementById("modal-caption").innerHTML = caption;
+    //var doc = document.getElementById("modal-caption").innerHTML = caption;
     document.getElementById("image").src = gpath;
-    
-  
-  
 }
 
 function decModal() {
     var path = document.getElementById("image").src;
+    
     console.log(path+' Image Path');
     
-    
     var newpath = path.split("/");
-    
     var length = newpath.length-1;
     
     //image index string
@@ -106,7 +109,7 @@ function decModal() {
     
     console.log(newpath[length]+' Last element of newpath array.');
     console.log(imIS[0]+' Image Index before decrement');
-
+    //if current index is min, do nothing, else decrease by one
     if(imIS[0]==(0)){
       var match = imIS[0]
       }
@@ -114,31 +117,22 @@ function decModal() {
     var match = Number(imIS[0])-1;
     }
     
-    
     console.log(match+' match');
-    // "/.$/" Replaces last character of string with specified string.
-    //var gpath = epath.replace(/.$/,String(match))+".jpg";
+    //join path to put into file
     imIS[0] = match;
     newpath[length]=imIS.join(".")
     var gpath = newpath.join("/")
+    
     console.log(gpath+' Final Path');
-    
+    //write path
     var caption = document.getElementById("image").alt;
-    var doc = document.getElementById("modal-caption").innerHTML = caption;
+    //var doc = document.getElementById("modal-caption").innerHTML = caption;
     document.getElementById("image").src = gpath;
-    
-  
-  
 }
 function closeModal() {
-  
-    var elem = document.getElementById("modal");
-    elem.style.display = "none";
-    var body = document.body;
-    body.style = "overflow:auto;"
+    document.getElementById("modal").style.display = "none";
+    document.body.style = "overflow:auto;"
 }
-
-//Please know enough to not have to have this function explained.
 
 
 /*
